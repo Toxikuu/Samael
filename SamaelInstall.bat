5e8af6d9b235de5566f8e04c982682edce796eef
Toxikuu <140706894+Toxikuu@users.noreply.github.com>
2024-02-16 21:14:39 -0600

    Update SamaelInstall.bat
    v5.1.2

--- SamaelInstall.bat
+++ SamaelInstall.bat
@@ -1,31 +1,36 @@
 @echo off
 
-rem Define the Python version to install
-set PYTHON_VERSION=3.12.2
-echo Downloading python version %PYTHON_VERSION%
+rem Check if Python is already installed
+python --version > nul 2>&1
+if %errorlevel%==0 (
+    echo Python is already installed.
+) else (
+	rem Define the Python version to install
+	set PYTHON_VERSION=3.12.2
+	echo Downloading python version %PYTHON_VERSION%
 
-rem Define the installation directory
-set INSTALL_DIR=C:\Python\%PYTHON_VERSION%
-echo Installation directory: %INSTALL_DIR%
+	rem Define the installation directory
+	set INSTALL_DIR=C:\Python\%PYTHON_VERSION%
+	echo Installation directory: %INSTALL_DIR%
 
-rem Define the download URL for Python
-set DOWNLOAD_URL=https://www.python.org/ftp/python/%PYTHON_VERSION%/python-%PYTHON_VERSION%-amd64.exe
+	rem Define the download URL for Python
+	set DOWNLOAD_URL=https://www.python.org/ftp/python/%PYTHON_VERSION%/python-%PYTHON_VERSION%-amd64.exe
 
-rem Create the installation directory
-mkdir %INSTALL_DIR%
+	rem Create the installation directory
+	mkdir %INSTALL_DIR%
 
-rem Download Python installer using PowerShell
-echo Downloading Python with PowerShell
-powershell -Command "Invoke-WebRequest -Uri '%DOWNLOAD_URL%' -OutFile 'python-installer.exe'"
+	rem Download Python installer using PowerShell
+	echo Downloading Python with PowerShell
+	powershell -Command "Invoke-WebRequest -Uri '%DOWNLOAD_URL%' -OutFile 'python-installer.exe'"
 
-rem Install Python silently
-echo Installing Python
-python-installer.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+	rem Install Python silently
+	echo Installing Python
+	python-installer.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
 
-echo Deleting Python installer
-rem Remove the Python installer
-del python-installer.exe
-
+	echo Deleting Python installer
+	rem Remove the Python installer
+	del python-installer.exe
+)
 
 
 rem Install Samael dependencies
@@ -55,11 +60,13 @@ rem Create the destination directory if it doesn't exist
 mkdir "%DESTINATION_DIR%" 2>nul
 
 rem Define the URL of the raw file on GitHub
-set FILE_URL=https://raw.githubusercontent.com/Toxikuu/Samael/%SAMAEL_VERSION%/Samael.py
+set SAMAEL_URL=https://raw.githubusercontent.com/Toxikuu/Samael/%SAMAEL_VERSION%/Samael.py
+set TOX_ASSETS_URL=https://raw.githubusercontent.com/Toxikuu/Samael/%SAMAEL_VERSION%/tox_assets.py
 
-echo Downloading Samael.py to %DESTINATION_DIR%\Samael.py with PowerShell
+echo Downloading Samael.py and tox_assets.py to %DESTINATION_DIR% with PowerShell
 rem Download the file using PowerShell
-powershell -Command "Invoke-WebRequest -Uri '%FILE_URL%' -OutFile '%DESTINATION_DIR%\Samael.py'"
+powershell -Command "Invoke-WebRequest -Uri '%SAMAEL_URL%' -OutFile '%DESTINATION_DIR%\Samael.py'"
+powershell -Command "Invoke-WebRequest -Uri '%TOX_ASSETS_URL%' -OutFile '%DESTINATION_DIR%\tox_assets.py'"
 
 rem Check PowerShell exit code
 if %errorlevel%==0 (
