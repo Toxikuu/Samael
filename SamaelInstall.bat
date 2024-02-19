3b59551fac0123ed53b13497c0dc9d7c00764cf6
Toxikuu <140706894+Toxikuu@users.noreply.github.com>
2024-02-19 17:50:38 -0600

    Update SamaelInstall.bat
    v5.1.3

--- SamaelInstall.bat
+++ SamaelInstall.bat
@@ -1,5 +1,8 @@
 @echo off
 
+rem Define PowerShell path
+set PS=C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe
+
 rem Define the Python version to install
 set PYTHON_VERSION=3.12.2
 
@@ -12,11 +15,19 @@ set DOWNLOAD_URL=https://www.python.org/ftp/python/%PYTHON_VERSION%/python-%PYTH
 rem Create the installation directory
 mkdir %INSTALL_DIR%
 
+echo Installing Python %PYTHON_VERSION%
+echo
+echo
+echo
+
 rem Download Python installer using PowerShell
-powershell -Command "Invoke-WebRequest -Uri '%DOWNLOAD_URL%' -OutFile 'python-installer.exe'"
+%PS% -Command "Invoke-WebRequest -Uri '%DOWNLOAD_URL%' -OutFile 'python-installer.exe'"
 
 rem Install Python silently
 python-installer.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+
+rem Refresh path
+set "PATH=%PATH%"
 
 rem Check if Python was installed successfully by running `python --version`
 python --version > nul 2>&1
@@ -56,7 +67,7 @@ echo Dependencies installed.
 
 echo Downloading Samael from Github...
 rem Define Samael version
-set SAMAEL_VERSION=5.1.0
+set SAMAEL_VERSION=v5.1.3
 
 rem Define the destination directory using the current user's profile directory
 set DESTINATION_DIR=%USERPROFILE%\Samael
@@ -65,16 +76,21 @@ rem Create the destination directory if it doesn't exist
 mkdir "%DESTINATION_DIR%" 2>nul
 
 rem Define the URL of the raw file on GitHub
-set FILE_URL=https://raw.githubusercontent.com/Toxikuu/Samael/%SAMAEL_VERSION%/Samael.py
+set SAMAEL_URL=https://raw.githubusercontent.com/Toxikuu/Samael/%SAMAEL_VERSION%/Samael.py
+set CONFIG_URL=https://raw.githubusercontent.com/Toxikuu/Samael/%SAMAEL_VERSION%/config.ini
+set YAML_URL=https://github.com/Toxikuu/Samael/releases/download/%SAMAEL_VERSION%/stat_settings.yaml
+set TA_URL=https://raw.githubusercontent.com/Toxikuu/Samael/%SAMAEL_VERSION%/tox_assets.py
 
-rem Download the file using PowerShell
-powershell -Command "Invoke-WebRequest -Uri '%FILE_URL%' -OutFile '%DESTINATION_DIR%\Samael.py'"
+rem Download files using PowerShell
+%PS% -Command "Invoke-WebRequest -Uri '%SAMAEL_URL%' -OutFile '%DESTINATION_DIR%\Samael.py'"
+%PS% -Command "Invoke-WebRequest -Uri '%CONFIG_URL%' -OutFile '%DESTINATION_DIR%\config.ini'"
+%PS% -Command "Invoke-WebRequest -Uri '%YAML_URL%' -OutFile '%DESTINATION_DIR%\stat_settings.yaml'"
+%PS% -Command "Invoke-WebRequest -Uri '%TA_URL%' -OutFile '%DESTINATION_DIR%\tox_assets.py'"
 
 rem Check PowerShell exit code
 if %errorlevel%==0 (
     echo Samael downloaded successfully.
     echo Now run %DESTINATION_DIR%\Samael.py
-    echo After running Samael.py, you'll have to edit the config. Then you'll be done!
     pause
     exit /b 0
 ) else (
